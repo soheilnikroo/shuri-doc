74254ced2338cf2142baf160d02c2b57a7755a8a
Soheil Nikroo <soheiln1234@gmail.com>
2025-06-19 12:17:33 +0330

    Deploy website - based on 4982302dbe70f57cb508a87d118596d4a5116c7e

--- assets/js/614db6c2.4f52b691.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkshu_doc=self.webpackChunkshu_doc||[]).push([[491],{3451:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/shuri-doc/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/734a33bd.793c41dd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkshu_doc=self.webpackChunkshu_doc||[]).push([[872],{5539:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/shuri-doc/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
